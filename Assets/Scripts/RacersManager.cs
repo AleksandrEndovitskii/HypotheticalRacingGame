49b22af9d4707e23c3a962351319c6c60ac697f2
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2022-02-05 13:07:38 +0300

    Replace global racerIndex by local racerIndexes for each iteration

--- Assets/Scripts/RacersManager.cs
+++ Assets/Scripts/RacersManager.cs
@@ -13,8 +13,7 @@ public class RacersManager : MonoBehaviour
         var racersNeedingRemoved = new List<Racer>();
 
         // Updates the racers that are alive
-        var racerIndex = 0;
-        for (racerIndex = 1; racerIndex <= 1000; racerIndex++)
+        for (var racerIndex = 1; racerIndex <= 1000; racerIndex++)
         {
             if (racerIndex <= racers.Count)
             {
@@ -47,7 +46,7 @@ public class RacersManager : MonoBehaviour
 
         // Gets the racers that are still alive
         var newRacerList = new List<Racer>();
-        for (racerIndex = 0; racerIndex != racers.Count; racerIndex++)
+        for (var racerIndex = 0; racerIndex != racers.Count; racerIndex++)
         {
             // check if this racer must be removed
             if (racersNeedingRemoved.IndexOf(racers[racerIndex]) < 0)
@@ -57,7 +56,7 @@ public class RacersManager : MonoBehaviour
         }
 
         // Get rid of all the exploded racers
-        for (racerIndex = 0; racerIndex != racersNeedingRemoved.Count; racerIndex++)
+        for (var racerIndex = 0; racerIndex != racersNeedingRemoved.Count; racerIndex++)
         {
             var foundRacerIndex = racers.IndexOf(racersNeedingRemoved[racerIndex]);
             if (foundRacerIndex >= 0) // Check we've not removed this already!
@@ -69,12 +68,12 @@ public class RacersManager : MonoBehaviour
 
         // Builds the list of remaining racers
         racers.Clear();
-        for (racerIndex = 0; racerIndex < newRacerList.Count; racerIndex++)
+        for (var racerIndex = 0; racerIndex < newRacerList.Count; racerIndex++)
         {
             racers.Add(newRacerList[racerIndex]);
         }
 
-        for (racerIndex = 0; racerIndex < newRacerList.Count; racerIndex++)
+        for (var racerIndex = 0; racerIndex < newRacerList.Count; racerIndex++)
         {
             newRacerList.RemoveAt(0);
         }
